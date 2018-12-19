97dde57a362000efcedf70f8a4af125859f6e9d6
rokoruzic <roko.ruzic4@gmail.com>
2018-12-19 14:46:21 +0100

    moved classes and interfaces to folders

---
+++ Military/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Military
+{
+	public abstract  class Vehicle
+	{
+		public Guid  ID { get; set; }
+		public double Weight { get; set; }
+		public double AverageSpeed { get; set; }
+		public double FuelConsumption { get; set; }
+		public int Capacity { get; set; }
+		public override string ToString()
+		{
+			return $"ID: {ID} | Weight : {Weight} Average speed: {AverageSpeed} |Capacity: {Capacity}";
+
+		}
+	}
+}
